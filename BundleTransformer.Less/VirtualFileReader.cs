53d382cf3135a6d0172004d6ba7c7c420ebb715a
Andrey Taritsyn <taritsyn@gmail.com>
2012-05-16 20:17:20 +0400

    Version 1.3.0.0 Beta (added missing source files) 1. Added minifier-adapters based on the Google Closure Compiler and the Douglas Crockford JSMin 2. In BundleTransformer.Less replaced NuGet-package dotless to DotlessClientOnly and added support dotless 1.3.0.3 3. In BundleTransformer.MicrosoftAjax added support of Microsoft Ajax Minifier 4.52

---
+++ BundleTransformer.Less/VirtualFileReader.cs
@@ -0,0 +1,29 @@
+﻿namespace BundleTransformer.Less
+{
+	using System.IO;
+	using System.Web;
+
+	using dotless.Core.Input;
+
+	internal sealed class VirtualFileReader : IFileReader
+	{
+		public string GetFileContents(string fileName)
+		{
+			fileName = GetFullPath(fileName);
+
+			return File.ReadAllText(fileName);
+		}
+
+		public bool DoesFileExist(string fileName)
+		{
+			fileName = GetFullPath(fileName);
+
+			return File.Exists(fileName);
+		}
+
+		private static string GetFullPath(string path)
+		{
+			return HttpContext.Current.Server.MapPath(path);
+		}
+	}
+}
