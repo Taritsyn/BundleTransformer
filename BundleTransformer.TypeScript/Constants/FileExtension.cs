241b5b21a1da5c3a118f505750e72fa56a356eb4
Taritsyn <taritsyn@gmail.com>
2013-08-04 21:50:37 +0400

    Version 1.7.21.0 (In BundleTransformer.TypeScript added files: FileExtensionHelper.cs and FileExtension.cs)

---
+++ BundleTransformer.TypeScript/Constants/FileExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace BundleTransformer.TypeScript.Constants
+{
+	/// <summary>
+	/// File extensions constants
+	/// </summary>
+	internal static class FileExtension
+	{
+		/// <summary>
+		/// JavaScript-file extension
+		/// </summary>
+		public const string JavaScript = ".js";
+
+		/// <summary>
+		/// TypeScript-file extension
+		/// </summary>
+		public const string TypeScript = ".ts";
+	}
+}
---
+++ BundleTransformer.TypeScript/FileExtensionHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace BundleTransformer.TypeScript
+{
+	using System;
+
+	using Constants;
+
+	/// <summary>
+	/// File extension helper
+	/// </summary>
+	internal static class FileExtensionHelper
+	{
+		/// <summary>
+		/// Checks whether specified file extension is the JavaScript file extension 
+		/// </summary>
+		/// <param name="fileExtension">File extension</param>
+		/// <returns>Result of check</returns>
+		public static bool IsJavaScript(string fileExtension)
+		{
+			bool isJavaScript = string.Equals(fileExtension, FileExtension.JavaScript, StringComparison.OrdinalIgnoreCase);
+
+			return isJavaScript;
+		}
+
+		/// <summary>
+		/// Checks whether specified file extension is the TypeScript file extension 
+		/// </summary>
+		/// <param name="fileExtension">File extension</param>
+		/// <returns>Result of check</returns>
+		public static bool IsTypeScript(string fileExtension)
+		{
+			bool isTypeScript = string.Equals(fileExtension, FileExtension.TypeScript, StringComparison.OrdinalIgnoreCase);
+
+			return isTypeScript;
+		}
+	}
+}
