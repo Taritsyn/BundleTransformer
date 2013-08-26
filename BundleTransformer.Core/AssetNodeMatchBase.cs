dd16d6fd21000ce4e55ceadb8ee6695ed3a18ea1
Taritsyn <taritsyn@gmail.com>
2013-08-26 21:31:39 +0400

    Version 1.7.27.0 (Added file AssetNodeMatchBase.cs)

---
+++ BundleTransformer.Core/AssetNodeMatchBase.cs
@@ -0,0 +1,42 @@
+﻿namespace BundleTransformer.Core
+{
+	using System.Text.RegularExpressions;
+
+	/// <summary>
+	/// Asset node match
+	/// </summary>
+	public abstract class AssetNodeMatchBase
+	{
+		/// <summary>
+		/// Gets a position in the original string where 
+		/// the first character of the captured substring was found
+		/// </summary>
+		public int Position
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// Gets a single regular expression match
+		/// </summary>
+		public Match Match
+		{
+			get;
+			private set;
+		}
+
+
+		/// <summary>
+		/// Constructs instance of asset node match
+		/// </summary>
+		/// <param name="position">Position in the original string where 
+		/// the first character of the captured substring was found</param>
+		/// <param name="match">Single regular expression match</param>
+		protected AssetNodeMatchBase(int position, Match match)
+		{
+			Position = position;
+			Match = match;
+		}
+	}
+}
