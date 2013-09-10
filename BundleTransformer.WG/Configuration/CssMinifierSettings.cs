b955779a1f75857db6b3aaed9f46a3425b750233
Taritsyn <taritsyn@gmail.com>
2013-09-10 18:36:29 +0400

    Version 1.8.2.0 (In BundleTransformer.WG was added configuration classes)

---
+++ BundleTransformer.WG/Configuration/CssMinifierSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace BundleTransformer.WG.Configuration
+{
+	using System.Configuration;
+
+	/// <summary>
+	/// Configuration settings of WebGrease Semantic CSS-minifier
+	/// </summary>
+	public sealed class CssMinifierSettings : ConfigurationElement
+	{
+		/// <summary>
+		/// Gets or sets a flag for whether to enable usual minification
+		/// </summary>
+		[ConfigurationProperty("shouldMinify", DefaultValue = true)]
+		public bool ShouldMinify
+		{
+			get { return (bool)this["shouldMinify"]; }
+			set { this["shouldMinify"] = value; }
+		}
+	}
+}
---
+++ BundleTransformer.WG/Configuration/WgSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace BundleTransformer.WG.Configuration
+{
+	using System.Configuration;
+
+	/// <summary>
+	/// Configuration settings of WebGrease Minifier
+	/// </summary>
+	public sealed class WgSettings : ConfigurationSection
+	{
+		/// <summary>
+		/// Gets a configuration settings of WebGrease Semantic CSS-minifier
+		/// </summary>
+		[ConfigurationProperty("css")]
+		public CssMinifierSettings CssMinifier
+		{
+			get { return (CssMinifierSettings)this["css"]; }
+		}
+	}
+}
